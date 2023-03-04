76036939e2f340187fd4471a5c46b81885377d1e
yash <yashwanthbs55@gmail.com>
2023-03-04 22:10:28 +0530

    new file

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!bin/bash
+################
+#script to check the EC2 resources using during a particular time through cronjob
+###############
+
+#Things we are tracking
+#EC2 insances, lamba functions, s3 buckets, EBS volumes
+
+echo "The EC2 instances in the account are:"
+aws ec2 describe-instances |jq '.Reservations[].Instances[].InstanceId'
+
+echo "The lamba functions in the account are:"
+aws lambda list-functions|jq '.Functions[].FunctionName'
+
+echo "The EBS volumes in the account are:"
+aws ec2 describe-volumes|jq '.Volumes[].InstanceId'
+echo "state of volume:"
+aws ec2 describe-volume |jq '.Volumes[].State'
+
